0b984279ae2e15a350c3aea4d1ce271e533d6ac8
João Oliveira <jvmdo.eng16@uea.edu.br>
2021-08-19 22:32:38 +0000

    flask hello world

--- app.py
+++ app.py
@@ -1,1 +1,7 @@
-print("Hello World from Gitpod!")
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/hello")
+def initial_page():
+    return "Hello World this is my first Flask application!"
