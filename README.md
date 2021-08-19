797e393d095ec4d7d34e8c80fb804d4ed29b7616
João Oliveira <jvmdo.eng16@uea.edu.br>
2021-08-19 23:55:45 +0000

    add scheme and post page template

--- README.md
+++ README.md
@@ -3,3 +3,9 @@ Repo for Ocean python html css course
 
 # Link to Gitpod
 [Gitpod editor for this repo](https://beige-deer-63cesbgj.ws-us15.gitpod.io/)
+
+# Bom Lembrar
+O arquivo de requerimentos é criado utilizando o comando
+```bash
+pip freeze > requirements.txt
+```
--- app.py
+++ app.py
@@ -1,7 +1,28 @@
-from flask import Flask
+import sqlite3
+from flask import Flask, request, session, g, redirect, abort, render_template
+
+# settings 
+DATABASE = 'blog.db'
+SECRET_KEY = 'flan'
 
 app = Flask(__name__)
+app.config.from_object(__name__)
+
+def db_connect():
+    return sqlite3.connect(app.config['DATABASE'])
+
+@app.before_request
+def before_req():
+    g.db = db_connect()
+
+@app.teardown_request
+def after_req(exc):
+    g.db.close()
+
+@app.route('/')
+def display_posts():
+    return render_template('display_posts.html')
 
 @app.route("/hello")
-def initial_page():
+def hello_page():
     return "Hello World this is my first Flask application!"
